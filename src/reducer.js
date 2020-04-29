448283218755d530c8a3b812c4b546190c21dffd
Kim Ly Buck <kim.3993@gmail.com>
2020-04-28 19:30:58 -0500

    created store, reducer, store

---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+export const initialState = {
+	additionalPrice: 0,
+	car: {
+		price: 26395,
+		name: '2019 Ford Mustang',
+		image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+		features: []
+	},
+	additionalFeatures: [
+		{ id: 1, name: 'V-6 engine', price: 1500 },
+		{ id: 2, name: 'Racing detail package', price: 1500 },
+		{ id: 3, name: 'Premium sound system', price: 500 },
+		{ id: 4, name: 'Rear spoiler', price: 250 }
+	]
+};
+
+export default function reducer(state, action) {
+
+    let feature;
+	switch (action.type) {
+		case 'ADD_FEATURE':
+            // f is the feature object
+            // find() return the object when return is true.
+            feature = state.additionalFeatures.find(f => {
+                return f.id === action.id
+            })
+			return {
+                ...state,
+                additionalPrice: state.additionalPrice + feature.price,
+				car: {
+					...state.car,
+					features: [ ...state.car.features, action.id]
+				}
+            };
+        default: return state;
+	}
+}
